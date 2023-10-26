5cacbb6177d6bc5f8c7a42e4dcad933136331c19
Anshu Pandey <anshuspandey@gmail.com>
2023-10-26 17:18:10 +0530

    some changes

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,6 +1,8 @@
 from model.train import get_csvs_df
 import os
 import pytest
+from src.model.train import train_model
+import numpy as np
 
 
 def test_csvs_no_files():
@@ -20,3 +22,13 @@ def test_csvs_creates_dataframe():
     datasets_directory = os.path.join(current_directory, 'datasets')
     result = get_csvs_df(datasets_directory)
     assert len(result) == 20
+
+
+def test_train_model():
+    X_train = np.array([1,2,3,4,5,6]).reshape(-1,1)
+    y_train = np.array(10,9,8,7,6,5)
+    data = {"train":{"X":X_train,"y":y_train}}
+
+    reg_model = train_model(data,{"alpha":1.2})
+    preds = reg_model.predict([[1],[2]])
+    np.testing.assert_almost_equal(preds,[9.93939393,9.0303030303])
